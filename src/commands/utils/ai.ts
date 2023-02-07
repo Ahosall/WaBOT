8f624c71758b7e60d93272d9cc61d67810ebdf2b
Ahosall <zen.wixyos.br@gmail.com>
2023-02-07 14:29:07 -0400

    Adicionado função de dialogo na IA

--- src/commands/utils/ai.ts
+++ src/commands/utils/ai.ts
@@ -3,6 +3,40 @@ import Message from "../../types/Message";
 
 import openAI from "../../services/openai";
 
+const messages = [];
+const defaults = [
+  "IA: Eu sou a Ayee.",
+  "IA: Fui criada por Ahosall.",
+  "IA: Ahosall é um jovem desenvolvedor de 17 anos, e é dono do seguinte perfil do github: https://github.com/ahosall",
+];
+
+const chatgpt = async (author: string, entry: string) => {
+  messages.push({ author, content: entry });
+
+  return await openAI
+    .createCompletion({
+      model: "text-davinci-003",
+      prompt: [
+        defaults.join("\n"),
+        messages.map((e) => `${e.author}: ${e.content}\nIA:`).join("\n"),
+      ].join(" "),
+      temperature: 0.9,
+      max_tokens: 1024,
+    })
+    .then((res) => {
+      const choices = res.data.choices;
+      const selected = choices[0].text
+        .replace("\n", "")
+        .replace("\n", "")
+        .replace("IA: ", "")
+        .replace("  ", "");
+
+      const threat = selected.replace(" ", "") !== "" ? selected : "Não sei...";
+      messages.push({ author: "IA", content: threat });
+      return threat;
+    });
+};
+
 module.exports = {
   info: {
     name: "ai",
@@ -10,19 +44,9 @@ module.exports = {
     description: "Faça uma pergunta para a inteligencia artifical",
   },
   run: async (client: Client, message: Message, args: Array<string>) => {
-    openAI
-      .createCompletion({
-        model: "text-davinci-003",
-        prompt: args.join(" "),
-        temperature: 0.9,
-        max_tokens: 1024,
-      })
-      .then((res) => {
-        const choices = res.data.choices;
-        const selected = choices[Math.floor(Math.random() * choices.length)];
-        const threat = selected.text.replace("\n", "").replace("\n", " ");
+    if (args.length < 1) return;
+    const res = await chatgpt(message.author.name, args.join(" "));
 
-        message.reply(threat);
-      });
+    message.reply(res);
   },
 };
