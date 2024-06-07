391860eb501eeff3b62139c208ae3f70f4ebf4e9
Ahosall <zen.wixyos.br@gmail.com>
2024-06-07 09:22:24 -0400

    Improve types

--- src/events/Messages.ts
+++ src/events/Messages.ts
@@ -12,13 +12,14 @@ const event: Events = {
   name: "messages.upsert",
   run: async (client, recv: TMessageUpsertProps) => {
     const { messages, type } = recv;
-    console.log(type, messages);
+
     if (type !== "notify") return;
     if (messages.length === 0) return;
 
     messages.forEach(async (m) => {
       console.log(JSON.stringify(m, null, 2));
       const message = new MessageDispatcher(client, m);
+      // console.log(JSON.stringify(message, null, 2));
     });
   },
 };
