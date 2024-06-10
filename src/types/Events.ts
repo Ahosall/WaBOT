f5f3666c93a6c6722b14623a504881958bfad38d
Ahosall <zen.wixyos.br@gmail.com>
2024-06-10 10:46:26 -0400

    Document types

--- src/types/Events.ts
+++ src/types/Events.ts
@@ -1,8 +1,23 @@
 import { BaileysEventMap } from "@whiskeysockets/baileys";
 import Client from "../utils/Instance";
 
+/**
+ * Type definition for an event in the Baileys library.
+ */
 type Events = {
+  /**
+   * The name of the event.
+   * This should be a key from the BaileysEventMap, which defines all possible events.
+   */
   name: keyof BaileysEventMap;
+
+  /**
+   * The function to be executed when the event is triggered.
+   *
+   * @param client - The client instance that interacts with the Baileys library.
+   * @param args - The arguments associated with the event, can be of any type.
+   * @returns A promise that resolves to void or any value.
+   */
   run: (client: Client, args: any) => Promise<void | any>;
 };
 
