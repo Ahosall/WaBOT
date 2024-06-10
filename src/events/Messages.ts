1c10506d3f43a87a8cad73d5db184c7962ceb375
Ahosall <zen.wixyos.br@gmail.com>
2024-06-10 10:37:37 -0400

    Add check for ignore status, improve command execution structure

--- src/events/Messages.ts
+++ src/events/Messages.ts
@@ -14,10 +14,21 @@ const event: Events = {
     messages.forEach(async (m) => {
       const message = new MessageDispatcher(client, m);
 
+      if (message.from === "status@broadcast") return;
       if (message.content === null) return;
       if (!message.content?.startsWith(client.prefix)) return;
 
-      console.log(message.content);
+      const body = message.content.slice(client.prefix.length).split(" ");
+
+      const cmd = body[0];
+      const args = body.slice(1);
+
+      if (!cmd) return;
+
+      const command = client.commands.get(cmd) || client.aliases.get(cmd);
+      if (!command) return;
+
+      await command.run(client, message, args);
     });
   },
 };
